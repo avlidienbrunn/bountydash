9262906541df45637384e96279623b1a3a9c3e10
Mathias Karlsson <avlidienbrunn@gmail.com>
2017-03-16 18:04:00 +0100

    added bugs/amount left on forecast

--- assets/main.js
+++ assets/main.js
@@ -218,11 +218,17 @@ function stats() {
 
 	current_year = new Date().getFullYear();
 	current_months = new Date().getMonth()+1;
+	
+	forecast_rewarded = (years[current_year]['reports'] / current_months) * 12;
+	left_rewarded = forecast_rewarded - years[current_year]['reports'];
+	forecast_amount = (years[current_year]['amount'] / current_months) * 12;
+	left_amount = (forecast_amount - years[current_year]['amount']).toFixed(2);
+
 	$('#stats-forecast').html("");
 	row = $("<tr>")
 		.append($('<td>').text(years[current_year].year))
-		.append($('<td>').text((years[current_year]['reports'] / current_months) * 12))
-		.append($('<td>').text((years[current_year]['amount'] / current_months) * 12+" "+years[current_year]['currency']))
+		.append($('<td title="'+left_rewarded+' left">').text(forecast_rewarded))
+		.append($('<td title="'+left_amount+' left">').text(forecast_amount+" "+years[current_year]['currency']))
 	$('#stats-forecast').append(row);
 
 	if(typeof year !== 'undefined') {
